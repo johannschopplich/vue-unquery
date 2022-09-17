70975ba1aa74cb91eb087c4a834d706673964895
Johann Schopplich <mail@johannschopplich.com>
2022-09-17 10:06:34 +0200

    feat: refetch on focus & network changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { abort, forget, mutate, query, subscribe } from 'turbo-query'
+import { abort, expiration, forget, mutate, query, subscribe } from 'turbo-query'
 import { computed, getCurrentInstance, inject, onUnmounted, readonly, ref, watch } from 'vue'
-import type { TurboMutateValue, TurboQueryOptions } from 'turbo-query'
-import type { InjectionKey } from 'vue'
+import type { TurboMutateValue } from 'turbo-query'
+import type { InjectionKey, Ref } from 'vue'
 import type { TurboVueKey, TurboVueOptions, TurboVueResource } from './types'
 
 export * from './types'
@@ -28,6 +28,11 @@ export async function useAsyncQuery<T = any>(
   const turboSubscribe = options?.turbo?.subscribe ?? contextOptions?.turbo?.subscribe ?? subscribe
   const turboForget = options?.turbo?.forget ?? contextOptions?.turbo?.forget ?? forget
   const turboAbort = options?.turbo?.abort ?? contextOptions?.turbo?.abort ?? abort
+  const refetchOnFocus = options?.refetchOnFocus ?? contextOptions?.refetchOnFocus ?? true
+  const refetchOnConnect = options?.refetchOnConnect ?? contextOptions?.refetchOnConnect ?? true
+  const focusInterval = options?.focusInterval ?? contextOptions?.focusInterval ?? 5000
+  const turboExpiration = options?.turbo?.expiration ?? contextOptions?.turbo?.expiration ?? expiration
+  const clearOnForget = options?.clearOnForget ?? contextOptions?.clearOnForget ?? false
 
   const computedKey = computed(() => {
     try {
@@ -58,17 +63,18 @@ export async function useAsyncQuery<T = any>(
   if (computedKey.value) {
     resource.value = await turboQuery<T>(computedKey.value, {
       stale: true,
+      ...contextOptions,
       ...options,
     })
   }
 
-  async function refetch(opts?: TurboQueryOptions): Promise<T | undefined> {
+  async function refetch(): Promise<T | undefined> {
     if (!computedKey.value)
       return
     return await turboQuery<T>(computedKey.value, {
       stale: false,
+      ...contextOptions,
       ...options,
-      ...opts,
     })
   }
 
@@ -88,6 +94,127 @@ export async function useAsyncQuery<T = any>(
     if (!computedKey.value)
       return
     turboAbort(computedKey.value, reason)
+  }
+
+  const lastFocus = ref(new Date())
+
+  function onFocus(listener: () => void): () => void {
+    if (!refetchOnFocus || typeof window === 'undefined')
+      return () => {}
+
+    const rawHandler = () => {
+      const last = lastFocus.value
+      const now = new Date()
+
+      if (now.getTime() - last.getTime() > focusInterval) {
+        lastFocus.value = new Date()
+        listener()
+      }
+    }
+
+    window.addEventListener('focus', rawHandler)
+    return () => window.removeEventListener('focus', rawHandler)
+  }
+
+  function onConnect(listener: () => void): () => void {
+    if (refetchOnConnect && typeof window !== 'undefined') {
+      window.addEventListener('online', listener)
+      return () => window.removeEventListener('online', listener)
+    }
+    return () => {}
+  }
+
+  function createFocusAvailable(
+    precision: number,
+  ): [Readonly<Ref<boolean>>, Readonly<Ref<number>>] {
+    const isAvailable = ref(new Date().getTime() - lastFocus.value.getTime() > focusInterval)
+    const availableIn = ref(focusInterval)
+
+    const interval = setInterval(() => {
+      const last = lastFocus.value
+      const now = new Date()
+      const availability = focusInterval - (now.getTime() - last.getTime())
+      if (availability >= 0)
+        availableIn.value = availability
+      else if (availability < 0 && availableIn.value > 0)
+        availableIn.value = 0
+      isAvailable.value = now.getTime() - last.getTime() > focusInterval
+    }, precision)
+
+    onUnmounted(() => {
+      clearInterval(interval)
+    })
+
+    return [isAvailable, availableIn]
+  }
+
+  /**
+   * Returns the expiration date of the current key.
+   * If the item is not in the cache, it will return undefined.
+   */
+  function localExpiration(): Date | undefined {
+    const key = computedKey.value
+    if (!key)
+      return undefined
+
+    return turboExpiration(key)
+  }
+
+  /**
+   * Creates a signal that every given pricesion interval
+   * will determine if the current key is currently expired / stale
+   * and how many time needs to pass till its considered expired / stale.
+   * This function helps creating
+   * the controlled sigal on demand rather than creating
+   * arbitrary signals ourselves just in case.
+   * Return value is [isStale, staleIn]
+   */
+  function createStale(precision: number): [Readonly<Ref<boolean>>, Readonly<Ref<number>>] {
+    const now = new Date()
+    const initialKey = computedKey.value
+
+    let initialIsStale = true
+    let initialStaleIn = 0
+
+    if (initialKey) {
+      const expiresAt = expiration(initialKey)
+      if (expiresAt) {
+        const expirationIn = expiresAt.getTime() - now.getTime()
+        if (expirationIn >= 0)
+          initialStaleIn = expirationIn
+        initialIsStale = expiresAt.getTime() < now.getTime()
+      }
+    }
+
+    const isStale = ref(initialIsStale)
+    const staleIn = ref(initialStaleIn)
+
+    const unsubscribe = watch(computedKey, (key, _old, onCleanup) => {
+      const interval = setInterval(() => {
+        if (!key)
+          return
+        const expiresAt = expiration(key)
+        if (expiresAt) {
+          const now = new Date()
+          const expirationIn = expiresAt.getTime() - now.getTime()
+          if (expirationIn >= 0)
+            staleIn.value = expirationIn
+          else if (expirationIn < 0 && staleIn.value > 0)
+            staleIn.value = 0
+          isStale.value = expiresAt.getTime() < now.getTime()
+        }
+      }, precision)
+
+      onCleanup(() => {
+        clearInterval(interval)
+      })
+    })
+
+    onUnmounted(() => {
+      unsubscribe()
+    })
+
+    return [isStale, staleIn]
   }
 
   const unsubscribe = watch(
@@ -131,8 +258,28 @@ export async function useAsyncQuery<T = any>(
         },
       )
 
+      const unsubscribeForgotten = turboSubscribe<T>(key, 'forgotten', () => {
+        if (clearOnForget)
+          resource.value = undefined
+      })
+
+      /**
+       * Subscribe to focus changes if needed.
+       */
+      const unsubscribeFocusRefetch = onFocus(() => {
+        refetch()
+      })
+
+      /**
+       * Subscribe to network connect changes if needed.
+       */
+      const unsubscribeConnectRefetch = onConnect(() => {
+        refetch()
+      })
+
       resource.value = await turboQuery<T>(key, {
         stale: true,
+        ...contextOptions,
         ...options,
       })
 
@@ -141,6 +288,9 @@ export async function useAsyncQuery<T = any>(
         unsubscribeRefetching()
         unsubscribeResolved()
         unsubscribeErrors()
+        unsubscribeForgotten()
+        unsubscribeFocusRefetch()
+        unsubscribeConnectRefetch()
       })
     },
     { immediate: true },
@@ -161,6 +311,10 @@ export async function useAsyncQuery<T = any>(
       unsubscribe,
       error: readonly(error),
       isRefetching: readonly(isRefetching),
+      lastFocus: readonly(lastFocus),
+      createFocusAvailable,
+      expiration: localExpiration,
+      createStale,
     },
   ]
 }
@@ -178,6 +332,11 @@ export function useQuery<T = any>(
   const turboSubscribe = options?.turbo?.subscribe ?? contextOptions?.turbo?.subscribe ?? subscribe
   const turboForget = options?.turbo?.forget ?? contextOptions?.turbo?.forget ?? forget
   const turboAbort = options?.turbo?.abort ?? contextOptions?.turbo?.abort ?? abort
+  const refetchOnFocus = options?.refetchOnFocus ?? contextOptions?.refetchOnFocus ?? true
+  const refetchOnConnect = options?.refetchOnConnect ?? contextOptions?.refetchOnConnect ?? true
+  const focusInterval = options?.focusInterval ?? contextOptions?.focusInterval ?? 5000
+  const turboExpiration = options?.turbo?.expiration ?? contextOptions?.turbo?.expiration ?? expiration
+  const clearOnForget = options?.clearOnForget ?? contextOptions?.clearOnForget ?? false
   const immediate = options?.immediate ?? contextOptions?.immediate ?? true
 
   const computedKey = computed(() => {
@@ -201,17 +360,18 @@ export function useQuery<T = any>(
       return
     resource.value = await turboQuery<T>(computedKey.value, {
       stale: true,
+      ...contextOptions,
       ...options,
     })
   })()
 
-  async function refetch(opts?: TurboQueryOptions): Promise<T | undefined> {
+  async function refetch(): Promise<T | undefined> {
     if (!computedKey.value)
       return
     return await turboQuery<T>(computedKey.value, {
       stale: false,
+      ...contextOptions,
       ...options,
-      ...opts,
     })
   }
 
@@ -231,6 +391,127 @@ export function useQuery<T = any>(
     if (!computedKey.value)
       return
     turboAbort(computedKey.value, reason)
+  }
+
+  const lastFocus = ref(new Date())
+
+  function onFocus(listener: () => void): () => void {
+    if (!refetchOnFocus || typeof window === 'undefined')
+      return () => {}
+
+    const rawHandler = () => {
+      const last = lastFocus.value
+      const now = new Date()
+
+      if (now.getTime() - last.getTime() > focusInterval) {
+        lastFocus.value = new Date()
+        listener()
+      }
+    }
+
+    window.addEventListener('focus', rawHandler)
+    return () => window.removeEventListener('focus', rawHandler)
+  }
+
+  function onConnect(listener: () => void): () => void {
+    if (refetchOnConnect && typeof window !== 'undefined') {
+      window.addEventListener('online', listener)
+      return () => window.removeEventListener('online', listener)
+    }
+    return () => {}
+  }
+
+  function createFocusAvailable(
+    precision: number,
+  ): [Readonly<Ref<boolean>>, Readonly<Ref<number>>] {
+    const isAvailable = ref(new Date().getTime() - lastFocus.value.getTime() > focusInterval)
+    const availableIn = ref(focusInterval)
+
+    const interval = setInterval(() => {
+      const last = lastFocus.value
+      const now = new Date()
+      const availability = focusInterval - (now.getTime() - last.getTime())
+      if (availability >= 0)
+        availableIn.value = availability
+      else if (availability < 0 && availableIn.value > 0)
+        availableIn.value = 0
+      isAvailable.value = now.getTime() - last.getTime() > focusInterval
+    }, precision)
+
+    onUnmounted(() => {
+      clearInterval(interval)
+    })
+
+    return [isAvailable, availableIn]
+  }
+
+  /**
+   * Returns the expiration date of the current key.
+   * If the item is not in the cache, it will return undefined.
+   */
+  function localExpiration(): Date | undefined {
+    const key = computedKey.value
+    if (!key)
+      return undefined
+
+    return turboExpiration(key)
+  }
+
+  /**
+   * Creates a signal that every given pricesion interval
+   * will determine if the current key is currently expired / stale
+   * and how many time needs to pass till its considered expired / stale.
+   * This function helps creating
+   * the controlled sigal on demand rather than creating
+   * arbitrary signals ourselves just in case.
+   * Return value is [isStale, staleIn]
+   */
+  function createStale(precision: number): [Readonly<Ref<boolean>>, Readonly<Ref<number>>] {
+    const now = new Date()
+    const initialKey = computedKey.value
+
+    let initialIsStale = true
+    let initialStaleIn = 0
+
+    if (initialKey) {
+      const expiresAt = expiration(initialKey)
+      if (expiresAt) {
+        const expirationIn = expiresAt.getTime() - now.getTime()
+        if (expirationIn >= 0)
+          initialStaleIn = expirationIn
+        initialIsStale = expiresAt.getTime() < now.getTime()
+      }
+    }
+
+    const isStale = ref(initialIsStale)
+    const staleIn = ref(initialStaleIn)
+
+    const unsubscribe = watch(computedKey, (key, _old, onCleanup) => {
+      const interval = setInterval(() => {
+        if (!key)
+          return
+        const expiresAt = expiration(key)
+        if (expiresAt) {
+          const now = new Date()
+          const expirationIn = expiresAt.getTime() - now.getTime()
+          if (expirationIn >= 0)
+            staleIn.value = expirationIn
+          else if (expirationIn < 0 && staleIn.value > 0)
+            staleIn.value = 0
+          isStale.value = expiresAt.getTime() < now.getTime()
+        }
+      }, precision)
+
+      onCleanup(() => {
+        clearInterval(interval)
+      })
+    })
+
+    onUnmounted(() => {
+      unsubscribe()
+    })
+
+    return [isStale, staleIn]
   }
 
   const unsubscribe = watch(
@@ -274,8 +555,28 @@ export function useQuery<T = any>(
         },
       )
 
+      const unsubscribeForgotten = turboSubscribe<T>(key, 'forgotten', () => {
+        if (clearOnForget)
+          resource.value = undefined
+      })
+
+      /**
+       * Subscribe to focus changes if needed.
+       */
+      const unsubscribeFocusRefetch = onFocus(() => {
+        refetch()
+      })
+
+      /**
+       * Subscribe to network connect changes if needed.
+       */
+      const unsubscribeConnectRefetch = onConnect(() => {
+        refetch()
+      })
+
       resource.value = await turboQuery<T>(key, {
         stale: true,
+        ...contextOptions,
         ...options,
       })
 
@@ -284,6 +585,9 @@ export function useQuery<T = any>(
         unsubscribeRefetching()
         unsubscribeResolved()
         unsubscribeErrors()
+        unsubscribeForgotten()
+        unsubscribeFocusRefetch()
+        unsubscribeConnectRefetch()
       })
     },
     { immediate },
@@ -304,6 +608,10 @@ export function useQuery<T = any>(
       unsubscribe,
       error: readonly(error),
       isRefetching: readonly(isRefetching),
+      lastFocus: readonly(lastFocus),
+      createFocusAvailable,
+      expiration: localExpiration,
+      createStale,
     },
   ]
 }
--- src/types.ts
+++ src/types.ts
@@ -5,50 +5,116 @@ export interface TurboVueOptions extends TurboQueryOptions {
   /**
    * A default turbo query instance to use if any.
    */
-  turbo?: TurboQuery
+  readonly turbo?: TurboQuery
+
+  /**
+   * Determines if it should refetch keys when
+   * the window regains focus. You can also
+   * set the desired `focusInterval`.
+   * @default true
+   */
+  readonly refetchOnFocus?: boolean
+
+  /**
+   * Determines if it should refetch keys when
+   * the window regains focus.
+   * @default true
+   */
+  readonly refetchOnConnect?: boolean
+
+  /**
+   * Determines a throttle interval for the
+   * `refetchOnFocus`.
+   * @default 5000
+   */
+  readonly focusInterval?: number
+
+  /**
+   * Clears the resource signal by setting it to
+   * undefined when the key is forgotten from the cache.
+   * @default false
+   */
+  readonly clearOnForget?: boolean
 
   /*
    * Prevent the request from firing until the `refetch` method is called
    * or the computed key changed. Only applies to non-suspense queries.
    */
-  immediate?: boolean
+  readonly immediate?: boolean
 }
 
 export interface TurboVueResourceActions<T> {
   /**
    * Refetches the current key.
    */
-  refetch(opts?: TurboQueryOptions): Promise<T | undefined>
+  readonly refetch: () => Promise<T | undefined>
 
   /**
    * Mutates the current key.
    */
-  mutate(value: TurboMutateValue<T>): void
+  readonly mutate: (value: TurboMutateValue<T>) => void
 
   /**
    * Usubscribes from the current key changes.
    */
-  unsubscribe(): void
+  readonly unsubscribe: () => void
 
   /**
    * Forgets the current key from the cache.
    */
-  forget(): void
+  readonly forget: () => void
 
   /**
    * Aborts the current key's request if any.
    */
-  abort(reason?: any): void
+  readonly abort: (reason?: any) => void
 
   /**
    * Determines if an error occured.
    */
-  error: Readonly<Ref<unknown>>
+  readonly error: Readonly<Ref<unknown>>
 
   /**
    * Determines if it's refetching in the background.
    */
-  isRefetching: Readonly<Ref<boolean>>
+  readonly isRefetching: Readonly<Ref<boolean>>
+
+  /**
+   * Determines the date of the last window focus.
+   * Useful to calculate how many time is left
+   * for the next available focus refetching.
+   */
+  readonly lastFocus: Readonly<Ref<Date>>
+
+  /**
+   * Creates a ref that every given precision interval
+   * will determine if the current key is available
+   * to refetch via focus and how many time needs to pass till
+   * it's available to refetch by focus. This function helps creating
+   * the controlled ref on demand rather than creating
+   * arbitrary refs ourselves just in case.
+   * Return value is [`isAvailable` (readonly ref), `availableIn` (readonly ref)]
+   */
+  readonly createFocusAvailable: (
+    precision: number
+  ) => [Readonly<Ref<boolean>>, Readonly<Ref<number>>]
+
+  /**
+   * Determines when the current key expires if
+   * it's currently in the cache.
+   */
+  readonly expiration: () => Date | undefined
+
+  /**
+   * Creates a signal that every given pricesion interval
+   * will determine if the current key is currently expired / stale
+   * and how many time needs to pass till its considered expired / stale.
+   * This function helps creating
+   * the controlled ref on demand rather than creating
+   * arbitrary refs ourselves just in case.
+   * Return value is [isStale (readonly ref), staleIn (readonly ref)]
+   */
+  readonly createStale: (precision: number) => [Readonly<Ref<boolean>>, Readonly<Ref<number>>]
 }
 
 export type TurboVueResource<T> = [
